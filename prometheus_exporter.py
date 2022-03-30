2e3cc0c957a08fa4310d338ee9cebf7b2d8fc2a5
JuMasta <nikita.bulatov@virtoway.com>
2022-03-30 18:51:08 +0200

    first commit

---
+++ prometheus_exporter.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client.core import CounterMetricFamily, HistogramMetricFamily, REGISTRY
+
+
+app = Flask(__name__)
+
+class CustomServiceExporter(object):
+    """docstring forCustomServiceExporter."""
+
+    stored_errors_count = {}
+
+    def collect(self):
+        errors_total = CounterMetricFamily(
+         "elasticsearch_logs_errors_total",
+         "Service HTTP elasticsearch_logs_errors_total",
+         labels=["errors"]
+        )
+
+        for errors, count in self.stored_errors_count.items():
+            errors_total.add_metric([errors],count)
+        yield errors_total
+
+
+
+REGISTRY.register(CustomServiceExporter())
+
+
+@app.route("/metric-reciever",methods=["POST"])
+def track_metric():
+    data = request.json
+    new_errors = data['errors']
+    CustomServiceExporter.stored_errors_count['errors'] = CustomServiceExporter \
+            .stored_errors_count.get('errors', 0) + new_errors
+
+    return Response(status=200)
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+
+app.run(host='0.0.0.0', port=80)
