b6ac82182785a08de0eeb26682f692579d741131
JuMasta <nikita.bulatov@virtoway.com>
2022-04-07 00:18:16 +0200

    feat: few logs level

--- prometheus_exporter.py
+++ prometheus_exporter.py
@@ -12,9 +12,17 @@ class CustomServiceExporter(object):
         errors_total = CounterMetricFamily(
          "elasticsearch_logs_errors_total",
          "Service HTTP elasticsearch_logs_errors_total",
-         labels=["pod_name","namespace_name"]
+         labels=["pod_name","namespace_name","level"]
         )
 
-        for pod, obj in self.stored_errors_count.items():
-            errors_total.add_metric([pod, obj['namespace_name'] ],obj['count'])
-        yield errors_total
+        # for pod, error_obj in self.stored_errors_count.items():
+        #     for level, data in error_obj:
+        #         print(pod)
+        #         # errors_total.add_metric([pod, data['namespace_name'],level ],data['count'])
+        #     yield errors_total
+        for pod in self.stored_errors_count:
+            for level in self.stored_errors_count[pod]:
+                namespace_name = self.stored_errors_count[pod][level]['namespace_name']
+                count = self.stored_errors_count[pod][level]['count']
+                errors_total.add_metric([pod, namespace_name ,level ],count)
+                yield errors_total
